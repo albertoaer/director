7f03b8ee290dd6e64310a09e5b5ec8e6d3f34d7a
Alberto Elorza <albertoelorza2002@gmail.com>
2023-10-12 00:48:23 +0200

    dont close window behaviour

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,15 +1,35 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
+use tauri::{SystemTray, SystemTrayEvent, Manager, SystemTrayMenu, CustomMenuItem};
 
 fn main() {
+    let menu = SystemTrayMenu::new().add_item(CustomMenuItem::new("exit", "Exit"));
+
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+        .system_tray(SystemTray::new().with_menu(menu))
+        .on_system_tray_event(|app, event| match event {
+            SystemTrayEvent::LeftClick { .. } => {
+                let window = app.get_window("main").unwrap();
+                window.show().unwrap();
+            },
+            SystemTrayEvent::MenuItemClick { id, .. } if id == "exit" => {
+                std::process::exit(0);
+            }
+            _ => {}
+        })
+        .on_window_event(|event| match event.event() {
+            tauri::WindowEvent::CloseRequested { api, .. } => {
+                event.window().hide().unwrap();
+                api.prevent_close();
+              }
+              _ => {}
+        })
+        .invoke_handler(tauri::generate_handler![])
+        .build(tauri::generate_context!())
+        .expect("error while building tauri application")
+        .run(|_app, event| match event {
+            tauri::RunEvent::ExitRequested { api, .. } => { api.prevent_exit(); },
+            _ => {}
+        })
 }
