88c7f439bbe3ecdfc712388e55d7999e26fd8818
Alberto Elorza <albertoelorza2002@gmail.com>
2023-11-26 20:00:37 +0100

    change css when theme changes, allowing realtime system theme change on the application

--- src/lib/Theme.svelte
+++ src/lib/Theme.svelte
@@ -2,11 +2,15 @@
   import { getCurrent } from "@tauri-apps/api/window";
   import { writable, type Writable } from "svelte/store";
 
-  let theme$: Writable<string> = writable('light');
+  let theme$: Writable<string> = writable('dark');
 
   export function setTheme(theme: string) {
     theme$.set(theme);
   }
+
+  getCurrent().onThemeChanged(ev => {
+    theme$.set(ev.payload);
+  })
 
   getCurrent().theme().then(theme => {
     if (theme)
